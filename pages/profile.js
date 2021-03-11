30257dddb380910d98028455256771214b3c3fda
DaeseongLee <hsl5539@gmail.com>
2021-03-11 17:09:12 +0900

    Create project

---
+++ pages/profile.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+const Profile = () => (
+           <AppLayout> <div>내 프로필</div></AppLayout>
+    );
+
+export default Profile;
---
+++ pages/signup.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+
+const Signup = () => (
+            <AppLayout><div>회원가입</div></AppLayout>
+    );
+
+export default Signup;
